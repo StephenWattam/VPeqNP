741920a79bbf148e121e0ff042d35a63d9b78486
Matthew Edwards <m.edwards7@lancs.ac.uk>
2012-10-06 11:38:07 +0100

    Now (maybe) fixed

---
+++ lib/speechtag.rb
@@ -0,0 +1,25 @@
+
+
+train = File.open('resources/pos.train.txt', 'r')
+assoc = Hash.new
+
+while (line = train.gets)
+  example = line.split
+  word = assoc[example[0]] || word = Hash.new
+  #puts "refound #{example[0]} #{word}" if word[example[1]] != nil
+  (word[example[1]])  ? (word[example[1]] += 1) :  (word[example[1]] = 1)
+  assoc[example[0]] = word
+end
+
+train.close
+
+test = File.open('resources/pos.test.txt', 'r')
+
+while (line = test.gets)
+  example = line.split
+  data = assoc[example[0]]
+  puts data
+  puts "#{example[0]} is probably #{data.keys.max_by{|k,v| v}} (#{data.values.max}/#{data.values.inject(:+)})" if data
+end
+
+test.close
