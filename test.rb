be7c24ec0ddc5bf02035aaf7e32b7e04717bfef2
Matthew Edwards <m.edwards7@lancs.ac.uk>
2012-10-13 11:22:36 +0100

    Added hybrid model

--- test.rb
+++ test.rb
@@ -17,25 +17,28 @@ tz = VPNP::RegexTokeniser.new(WORD_TAG_BROWN) # word tag word tag word tag
 # -----------------------------------------------------------------------------
 # Training
 #Build a corpus from the training data and tokeniser.
-#ts = VPNP::DirTokenSource.new(brownsource, 11, -1, tz)
-#c = VPNP::Corpus.new
-#c.add_all(ts)
+ts = VPNP::DirTokenSource.new(brownsource, 11, -1, tz)
+c = VPNP::Corpus.new
+c.add_all(ts)
 
 # Save or load the corpus for speed
-#c.save("./testing/test_corpus")
-c = VPNP::Corpus.load("./testing/test_corpus")
+c.save("./testing/test_corpus")
+#c = VPNP::Corpus.load("./testing/test_corpus")
 
 # -----------------------------------------------------------------------------
 # Create a new model
 simple        = VPNP::SimpleProbabalisticTagModel.new (c)
 hmm           = VPNP::MarkovTagModel.new              (c)
 beff          = VPNP::BestEffortTagModel.new          (c)
-morph         = VPNP::MorphologicalRuleTagModel.new( { /.*ly$/ => 'adv',
-                                                       /.*ing$/ => 'vb',
-                                                       /a/ => 'at2',
-                                                       /^a[nt]$/ => 'at',
-                                                       /^th(e(re)?|a[nt])$/ => 'at'
-                                                      } )
+
+rules         =   { /.*ly$/ => 'adv',
+                   /.*ing$/ => 'vb',
+                   /a/ => 'at2',
+                   /^a[nt]$/ => 'at',
+                   /^th(e(re)?|a[nt])$/ => 'at'
+                  }
+morph         = VPNP::MorphologicalRuleTagModel.new(rules)
+hybr          = VPNP::SimpleHybridModel.new(c,rules)
 
 # -----------------------------------------------------------------------------
 # Testing
@@ -47,3 +50,5 @@ ts = VPNP::DirTokenSource.new(brownsource, 0, 10, tz)
 puts "Simple: #{simple.evaluate(ts).round(2)}%"
 ts = VPNP::DirTokenSource.new(brownsource, 0, 10, tz)
 puts "Morph: #{morph.evaluate(ts).round(2)}%"
+ts = VPNP::DirTokenSource.new(brownsource, 0, 10, tz)
+puts "Hybr: #{hybr.evaluate(ts).round(2)}%"
